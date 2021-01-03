7e4eaead772c477a064db26168ec99d03d5ecc0e
Dawon <da6921@naver.com>
2021-01-03 23:37:38 +0900

    commit

--- Tutorial5/src/tutorial5/Main2.java
+++ Tutorial5/src/tutorial5/Main2.java
@@ -3,8 +3,20 @@ package tutorial5;
 public class Main2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String a = "I Love You.";
+		if(a.contains("Love"))//String에서만 사용가능한 contains
+		{
+			//포함하는 경우 실행되는 부분
+			System.out.println("Me Too.");
+			
+		}
+		else
+		{
+			//포함하지 않는 경우 실행되는 부분
+			System.out.println("I Hate You.");
+
+		}
 	}
 
 }
